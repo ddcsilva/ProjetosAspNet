e99da9d267cb842c9e5aa0b399d94e4cd0d663e9
Danilo Silva <danilo.silva@msn.com>
2021-02-07 21:30:11 -0300

    Finalização

---
+++ CadastroClientesAutoComplete/CadastroClientesAutoComplete/Models/Cidade.cs
@@ -0,0 +1,11 @@
+﻿namespace CadastroClientesAutoComplete.Models
+{
+    public class Cidade
+    {
+        public int Id { get; set; }
+        public int CodigoIBGE { get; set; }
+        public string UF { get; set; }
+        public bool IsCapital { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ CadastroClientesAutoComplete/CadastroClientesAutoComplete/Models/ClientesContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace CadastroClientesAutoComplete.Models
+{
+    public class ClientesContext : DbContext
+    {
+        public ClientesContext() : base("DbAutocomplete")
+        {
+            Database.Log = instrucao => System.Diagnostics.Debug.WriteLine(instrucao);
+            Database.SetInitializer<ClientesContext>(new CreateDatabaseIfNotExists<ClientesContext>());
+            Database.Initialize(false);
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Cidade> Cidades { get; set; }
+    }
+}
