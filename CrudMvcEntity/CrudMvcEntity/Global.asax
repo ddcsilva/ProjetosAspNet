b5e0761783e77ba437a1a56a8d6c0a85d2ab372f
Danilo Silva <danilo.silva@msn.com>
2021-02-07 22:07:23 -0300

    Criação do Projeto + Model + DbContext + String de Conexão + Controller + Scaffolding

---
+++ CrudMvcEntity/CrudMvcEntity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudMvcEntity.MvcApplication" Language="C#" %>
---
+++ CrudMvcEntity/CrudMvcEntity/Models/EscolaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CrudMvcEntity.Models
+{
+    public class EscolaContext : DbContext
+    {
+        public EscolaContext() : base("Escola")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
